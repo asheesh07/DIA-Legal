8731420736f4f3e1a8bf0c5aa0ce5e71bd2b63cd
ASHEESH DHAMACHARLA <asheeshdhamacharla@ASHEESHs-MacBook-Air.local>
2026-03-20 19:26:01 +0530

    config: point frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ 
+  baseURL: 'https://dia-legal-1.onrender.com/api/'
+})
 
 export const getCases = () => api.get('/cases')
 export const deleteCase = (id) => api.delete(`/cases/${id}`)
